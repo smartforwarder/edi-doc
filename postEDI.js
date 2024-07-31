5156d40f98aedca5b9629b048c761c61534d7799
Jiqing Yao <jiqingyao@gmail.com>
2024-07-31 03:54:35 -0400

    fix: add faker

--- postEDI.js
+++ postEDI.js
@@ -2,6 +2,8 @@
 const dotenv = require("dotenv");
 const axios = require("axios");
 const data = require("./data.json");
+const fs = require("fs");
+const { faker } = require("@faker-js/faker");
 const API = "http://localhost:1337";
 dotenv.config();
 
@@ -14,13 +16,33 @@ main();
 async function main() {
   const token = await getToken(API);
   console.log(`Token: ${token}`);
-  const { resp } = await axios.post(`${API}/v1/shipments`, data, {
+  const fakeData = await getFakeData();
+  const { data: resp } = await axios.post(`${API}/v1/shipments`, fakeData, {
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
   });
-  console.log(resp);
+  // write the fake data to a file
+  fs.writeFileSync("data.json", JSON.stringify(fakeData, null, 2));
+  if (resp.success) {
+    console.log("EDI document created successfully");
+  } else {
+    console.error("Failed to create EDI document");
+  }
+}
+
+async function getFakeData() {
+  data.mbl_no =
+    faker.string.alpha({ length: 4, casing: "upper" }) +
+    faker.string.numeric(6);
+  data.hbls = data.hbls.map((hbl) => {
+    hbl.hbl_no =
+      faker.string.alpha({ length: 4, casing: "upper" }) +
+      faker.string.numeric(6);
+    return hbl;
+  });
+  return data;
 }
 
 async function getToken() {
